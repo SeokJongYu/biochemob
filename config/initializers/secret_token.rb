2c147c5cbd6af1d704355fd99a10b5f9774e0440
SeokJongYu <seokjongyu@gmail.com>
2013-04-14 15:35:13 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biochemob::Application.config.secret_token = 'd2a9a44f2bf36312bf7cf44279b362251cbe536f8ea8c479f47a42cb76016e6b37c8304dd43b070888c659e59b0df82728b48767909cc31601ae95eb13ff69d7'
